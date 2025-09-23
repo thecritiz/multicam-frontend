e4d59f9c5858b87de62ec631b7b062335e6f21a0
thecritiz <thecritiz@gmail.com>
2025-09-23 10:44:43 +0530

    Debugged Deployement-React-dom

--- src/App.js
+++ src/App.js
@@ -2,61 +2,47 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import AccessPage from "./components/AccessPage";
 import Home from "./components/Home";
 import CameraGrid from "./components/CameraGrid";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import AccessPage from "./components/AccessPage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Access verification */}
-        <Route path="/access" element={<AccessPage />} />
+        {/* Access page is the entry point */}
+        <Route path="/" element={<AccessPage />} />
 
-        {/* Landing Page (protected) */}
+        {/* Home after verification */}
         <Route
-          path="/"
+          path="/home"
           element={
-            <RequireAuth>
-              <div className="min-h-screen flex flex-col bg-gray-50">
-                <Header />
-                <main className="flex-1 container mx-auto p-6">
-                  <Home />
-                </main>
-                <Footer />
-              </div>
-            </RequireAuth>
+            <div className="min-h-screen flex flex-col bg-gray-50">
+              <Header />
+              <main className="flex-1 container mx-auto p-6">
+                <Home />
+              </main>
+              <Footer />
+            </div>
           }
         />
 
-        {/* Meeting Room (protected) */}
+        {/* Meeting room */}
         <Route
           path="/room/:roomId"
           element={
-            <RequireAuth>
-              <div className="min-h-screen flex flex-col bg-gray-50">
-                <Header />
-                <main className="flex-1 container mx-auto p-6">
-                  <CameraGrid />
-                </main>
-                <Footer />
-              </div>
-            </RequireAuth>
+            <div className="min-h-screen flex flex-col bg-gray-50">
+              <Header />
+              <main className="flex-1 container mx-auto p-6">
+                <CameraGrid />
+              </main>
+              <Footer />
+            </div>
           }
         />
       </Routes>
     </BrowserRouter>
   );
-}
-
-// 🔒 Wrapper component to enforce access
-function RequireAuth({ children }) {
-  const verified = localStorage.getItem("verified") === "true";
-  if (!verified) {
-    window.location.href = "/access"; // redirect if not verified
-    return null;
-  }
-  return children;
 }
