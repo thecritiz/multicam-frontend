87f47f462138b97deb2df0d1090f06d032ed840a
thecritiz <thecritiz@gmail.com>
2025-09-23 11:39:26 +0530

    Debugged Deployement-React-dom

--- src/App.js
+++ src/App.js
@@ -2,27 +2,35 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// Import all necessary components
 import Home from "./components/Home";
 import CameraGrid from "./components/CameraGrid";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import AccessPage from "./components/AccessPage"; // <-- Import AccessPage
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Route for the Access Page at the root */}
+        <Route path="/" element={<AccessPage />} />
+
+        {/* Route for the Home Page */}
         <Route
-          path="/"
+          path="/home"
           element={
             <div className="min-h-screen flex flex-col bg-gray-50">
               <Header />
-              <main className="flex-1 container mx-auto p-6">
+              <main className="flex-1 container mx-auto p-6 flex items-center justify-center">
                 <Home />
               </main>
               <Footer />
             </div>
           }
         />
+
+        {/* Route for the Meeting Room */}
         <Route
           path="/room/:roomId"
           element={
