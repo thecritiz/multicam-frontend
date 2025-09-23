2a43b9ab7402f3c34a6697084b5899d2e080f5ea
thecritiz <thecritiz@gmail.com>
2025-09-23 12:13:14 +0530

    yes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,39 @@
-// App.js
+// src/App.js
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./components/Home";
-import CameraGrid from "./components/CameraGrid";
 import Header from "./components/Header";
+import Timeline from "./components/Timeline";
+import Workflow from "./components/Workflow";
 import Footer from "./components/Footer";
+import Home from "./components/Home";
+import MeetingRoom from "./components/MeetingRoom";
 
-export default function App() {
+function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Landing Page */}
         <Route
           path="/"
           element={
             <div className="min-h-screen flex flex-col bg-gray-50">
               <Header />
-              <main className="flex-1 container mx-auto p-6">
+              <main className="flex-1 container mx-auto p-6 space-y-12">
                 <Home />
+                <Timeline />
+                <Workflow />
               </main>
               <Footer />
             </div>
           }
         />
-        <Route
-          path="/room/:roomId"
-          element={
-            <div className="min-h-screen flex flex-col bg-gray-50">
-              <Header />
-              <main className="flex-1 container mx-auto p-6">
-                <CameraGrid />
-              </main>
-              <Footer />
-            </div>
-          }
-        />
+
+        {/* Meeting Room */}
+        <Route path="/room/:roomId" element={<MeetingRoom />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
+export default App;
